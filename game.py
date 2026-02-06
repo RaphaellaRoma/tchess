3ccd5afc952ef1e0da246ffe3975cd12bdd240d4
Raphaella Roma <raphysics.oficial@gmail.com>
2026-02-06 17:12:03 -0300

    testes

--- game.py
+++ game.py
@@ -7,8 +7,19 @@ def start():
     print("Olá! Bem-vindo ao TerminalChess")
     print("1 - Jogador (indisponível)")
     print("2 - Multijogador")
+    # Exemplo de impressão individual
+    print("\u2654")  # Rei Branco ♔
+    print("\u265F")  # Peão Preto ♟
+
     return input("Selecione o modo: ")
 
+def jogador_1():
+    origem = input("Jogador 1 (brancas) selecione uma peça ")
+    destino = input("Mova a peça ")
+
+def jogador_2():
+    origem = input("Jogador 2 (pretas) selecione uma peça ")
+    destino = input("Mova a peça ")
 
 while state != 'finish':
     if state == 'start':
@@ -18,6 +29,9 @@ while state != 'finish':
             modo = start()
 
         print(tabuleiro)
+
+        jogador_1()
+        jogador_2()
         state = 'finish'
 
     
